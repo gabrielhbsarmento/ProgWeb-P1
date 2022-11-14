26e1cd91527e66ce8cd6dc5d85c815c49ac44085
gabrielhbsarmento <gabriel.sarmento@ufms.br>
2022-11-14 13:23:24 -0300

    Estilizando e importanto componente imput

--- projeto-final/src/pages/login/index.js
+++ projeto-final/src/pages/login/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import {Container , Form} from './styles'
+import Input from '../../components/imput/index';
 const Login = () => {
     return ( 
         <Container>
             <Form>
-                <h1>Teste de Componentes Estilizados</h1>
+                <h1>Faça seu Login !</h1>
+                <Input/>
+                <Input/>
+                <div>
+                  <p>não possui conta?</p>
+                  <a>Cadastrar</a>
+                </div>
             </Form>
         </Container>
 
